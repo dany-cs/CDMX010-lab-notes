5c14d1482f71b509812852d92e3e3052d8ac49e9
dany correo <danycorreos96@gmail.com>
2021-04-12 19:26:01 -0500

    Este commit contiene el modal y el primer componente

---
+++ take-note/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import { CreateNote } from './Components/createNote/CreateNote';
+
+
+function App() {
+  return (
+    <>
+      <a href="#principal">Tomar Nota</a>
+      < CreateNote/>
+    </>
+  )
+}
+
+export default App;
